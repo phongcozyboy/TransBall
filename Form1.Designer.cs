db772443d51f01d953de3883f3ac412a8cd7e4d0
phongle <hphongug0210@gmail.com>
2019-04-22 10:55:12 +0700

    add file Transball

---
+++ Form1.Designer.cs
@@ -0,0 +1,133 @@
+﻿namespace TransBall
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lbBi = new System.Windows.Forms.Label();
+            this.txtBi = new System.Windows.Forms.TextBox();
+            this.btnXacNhan = new System.Windows.Forms.Button();
+            this.btnChay = new System.Windows.Forms.Button();
+            this.gbox = new System.Windows.Forms.GroupBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.lbBuoc = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // lbBi
+            // 
+            this.lbBi.AutoSize = true;
+            this.lbBi.Location = new System.Drawing.Point(12, 9);
+            this.lbBi.Name = "lbBi";
+            this.lbBi.Size = new System.Drawing.Size(82, 13);
+            this.lbBi.TabIndex = 0;
+            this.lbBi.Text = "Nhập vào số bi:";
+            // 
+            // txtBi
+            // 
+            this.txtBi.Location = new System.Drawing.Point(100, 6);
+            this.txtBi.Name = "txtBi";
+            this.txtBi.Size = new System.Drawing.Size(100, 20);
+            this.txtBi.TabIndex = 1;
+            // 
+            // btnXacNhan
+            // 
+            this.btnXacNhan.Location = new System.Drawing.Point(217, 6);
+            this.btnXacNhan.Name = "btnXacNhan";
+            this.btnXacNhan.Size = new System.Drawing.Size(75, 23);
+            this.btnXacNhan.TabIndex = 2;
+            this.btnXacNhan.Text = "xác nhận";
+            this.btnXacNhan.UseVisualStyleBackColor = true;
+            this.btnXacNhan.Click += new System.EventHandler(this.btnXacNhan_Click);
+            // 
+            // btnChay
+            // 
+            this.btnChay.Location = new System.Drawing.Point(298, 6);
+            this.btnChay.Name = "btnChay";
+            this.btnChay.Size = new System.Drawing.Size(75, 23);
+            this.btnChay.TabIndex = 3;
+            this.btnChay.Text = "chạy ";
+            this.btnChay.UseVisualStyleBackColor = true;
+            this.btnChay.Click += new System.EventHandler(this.btnChay_Click);
+            // 
+            // gbox
+            // 
+            this.gbox.Location = new System.Drawing.Point(15, 53);
+            this.gbox.Name = "gbox";
+            this.gbox.Size = new System.Drawing.Size(688, 284);
+            this.gbox.TabIndex = 4;
+            this.gbox.TabStop = false;
+            this.gbox.Text = "Mô phỏng di chuyển";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(407, 16);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(135, 13);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Số bước mà bi đã hoán vị: ";
+            // 
+            // lbBuoc
+            // 
+            this.lbBuoc.AutoSize = true;
+            this.lbBuoc.Location = new System.Drawing.Point(548, 16);
+            this.lbBuoc.Name = "lbBuoc";
+            this.lbBuoc.Size = new System.Drawing.Size(13, 13);
+            this.lbBuoc.TabIndex = 6;
+            this.lbBuoc.Text = "0";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(715, 362);
+            this.Controls.Add(this.lbBuoc);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.gbox);
+            this.Controls.Add(this.btnChay);
+            this.Controls.Add(this.btnXacNhan);
+            this.Controls.Add(this.txtBi);
+            this.Controls.Add(this.lbBi);
+            this.Name = "Form1";
+            this.Text = "Trans Balls";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lbBi;
+        private System.Windows.Forms.TextBox txtBi;
+        private System.Windows.Forms.Button btnXacNhan;
+        private System.Windows.Forms.Button btnChay;
+        private System.Windows.Forms.GroupBox gbox;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label lbBuoc;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,226 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TransBall
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        int nArr = 0;
+        int disWidth1 = 0;
+        int disWidth2 = 60;
+
+        private int[] arr;
+        private int[] arrBalls;
+        private Button[] arrButton;
+
+        private void btnXacNhan_Click(object sender, EventArgs e)
+        {
+
+            if (arrButton != null)
+                for (int i = 0; i < nArr; i++)
+                {
+                    gbox.Controls.Remove(arrButton[i]);
+                    disWidth1 = disWidth2 = 60;
+                }
+
+            try
+            {
+                nArr = (2 * int.Parse(txtBi.Text.Trim())) + 1;
+                if(int.Parse(txtBi.Text.Trim()) < 1 || int.Parse(txtBi.Text.Trim()) > 5)
+                {
+                    MessageBox.Show("số bi có thể từ 1 đến 5 bi");
+                    txtBi.Clear();
+                    txtBi.Focus();
+                    return;
+                }
+
+            }
+            catch
+            {
+                MessageBox.Show("hãy nhập 1 số nguyên !");
+                txtBi.Clear();
+                txtBi.Focus();
+                return;
+            }
+            arr = new int[nArr];
+            arrBalls = new int[nArr];
+            arrButton = new Button[nArr];
+
+            for (int i = 0; i < nArr; i++)
+                arr[i] = arrBalls[i] = 0;
+
+            for (int i = 0; i < nArr; i++)
+            {
+                if (i < (nArr / 2))
+                {
+                    arr[i] = 1;
+                    arrBalls[i] = 2;
+                }
+                else if (i > (nArr / 2))
+                {
+                    arr[i] = 2;
+                    arrBalls[i] = 1;
+                }
+            }
+
+            int dis = (gbox.Width / 2) - int.Parse(txtBi.Text.Trim()) * 60;
+
+            //khoi tao giao dien
+            for (int i = 0; i < nArr; i++)
+            {
+                Button btn = new Button();
+                btn.Width = btn.Height = 40;
+                if(i < nArr / 2)
+                {
+                    btn.BackColor = Color.Green;
+                    btn.Location = new Point(dis + disWidth1, gbox.Height / 2);
+                    gbox.Controls.Add(btn);
+                }
+                else if(i > nArr / 2)
+                {
+                    btn.BackColor = Color.Red;
+                    btn.Location = new Point(dis + disWidth1, gbox.Height / 2);
+                    gbox.Controls.Add(btn);
+                }
+                else
+                {
+                    btn.Location = new Point(dis + disWidth1, gbox.Height / 2);
+                    gbox.Controls.Add(btn);
+                }
+                arrButton[i] = btn;
+                disWidth1 += 60;
+            }
+        }
+
+        void swapButton(ref Button b1, ref Button b2, ref int a, ref int b)
+        {
+			int posRoot = b1.Location.Y;
+			while (b1.Location.Y > 70)
+			{
+				b1.Location = new Point(b1.Location.X, b1.Location.Y - 4);
+				b2.Location = new Point(b2.Location.X, b2.Location.Y + 4);
+				Application.DoEvents();
+				Thread.Sleep(20);
+			}
+
+			if (b1.Location.X < b2.Location.X)
+			{
+				int posTemp = b2.Location.X;
+				while (b1.Location.X != posTemp)
+				{
+					b1.Location = new Point(b1.Location.X + 4, b1.Location.Y);
+					b2.Location = new Point(b2.Location.X - 4, b2.Location.Y);
+					Application.DoEvents();
+					Thread.Sleep(20);
+				}
+			}
+			else if (b1.Location.X > b2.Location.X)
+			{
+				int posTemp = b2.Location.X;
+				while (b1.Location.X != posTemp)
+				{
+					b1.Location = new Point(b1.Location.X - 4, b1.Location.Y);
+					b2.Location = new Point(b2.Location.X + 4, b2.Location.Y);
+					Application.DoEvents();
+					Thread.Sleep(20);
+				}
+			}
+			while (b1.Location.Y != posRoot)
+			{
+				b1.Location = new Point(b1.Location.X, b1.Location.Y + 4);
+				b2.Location = new Point(b2.Location.X, b2.Location.Y - 4);
+				Application.DoEvents();
+				Thread.Sleep(20);
+			}
+
+			int tempInt = arr[a]; arr[a] = arr[b]; arr[b] = tempInt;
+			Button tempButton = b1; b1 = b2; b2 = tempButton;
+		}
+
+		private void btnChay_Click(object sender, EventArgs e)
+		{
+			int step = 0;
+			int start = 0;
+			int end = nArr - 1;
+
+			while (true)
+			{
+				bool checkLeft = false;
+				bool checkRight = false;
+				int posRoot = 0;
+				int min = int.MaxValue;
+				int posMin;
+
+				while (arr[posRoot] != 0) posRoot++;
+
+				posMin = posRoot;
+				step++;
+
+				if (step % 2 != 0)
+				{
+					for (int i = start; i < posRoot; i++)
+					{
+						if (arr[i] == 1 && (posRoot - i) < min)
+						{
+							min = posRoot - i;
+							posMin = i;
+							checkLeft = true;
+						}
+					}
+
+					if (!checkLeft)
+						posMin = posRoot + (nArr / 2);
+				}
+				else
+				{
+					for (int i = end; i > posRoot; i--)
+					{
+						if (arr[i] == 2 && Math.Abs(posRoot - i) < min)
+						{
+							min = Math.Abs(posRoot - i);
+							posMin = i;
+							checkRight = true;
+						}
+					}
+
+					if (!checkRight)
+						posMin = posRoot - (nArr / 2);
+				}
+
+				swapButton(ref arrButton[posRoot], ref arrButton[posMin], ref posRoot, ref posMin);
+
+
+				lbBuoc.Text = step.ToString();
+
+				if (arrBalls[start] == arr[start])
+					start++;
+
+				if (arrBalls[end] == arr[end])
+					end--;
+
+				bool checkOut = true;
+				for (int i = 0; i < nArr; i++)
+					if (arrBalls[i] != arr[i])
+						checkOut = false;
+
+				if (checkOut)
+					break;
+			}
+
+			MessageBox.Show("Hoàn thành !");
+		}
+	}
+}
